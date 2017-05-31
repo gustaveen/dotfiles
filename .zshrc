ddcf401714f9f653d87aaa8c0ca5a9be896fe911
Gustav <gustaveen@gmail.com>
2017-06-01 01:44:09 +0200

    Add setup

---
+++ .zshrc
@@ -0,0 +1,25 @@
+#
+# Executes commands at the start of an interactive session.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
+# Customize to your needs...
+
+# Activate vim zsh vim bindings
+bindkey -v
+
+# Allow comments
+set -k
+
+# Re-bind ctrl+r to search (but with pattern)
+bindkey '^R' history-incremental-pattern-search-backward
+
+[ -n "$PS1" ] && source ~/.bash_profile
+
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# general
+ln -sf ~/dotfiles/.paths ~/.paths
+ln -sf ~/dotfiles/.aliases ~/.aliases
+ln -sf ~/dotfiles/.exports ~/.exports
+ln -sf ~/dotfiles/.functions ~/.functions
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+
+# zsh / zepto
+# ln -sf ~/dotfiles/.zshrc ~/.zshrc
+# ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+
+# ln -sf ~/dotfiles/bin ~/bin
+
+# ssh
+# ln -sf ~/dotfiles/ssh/config ~/.ssh/config
+
+# tmux
+# ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# zsh
+# ln -sf ~/dotfiles/.zprofile ~/.zprofile
+
+# psql
+# ln -sf ~/dotfiles/.psqlrc ~/.psqlrc
+
+# git
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git/.gitignore ~/.gitignore
+
+# vim
+# ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+# mkdir -p ~/.config/nvim
+# ln -sf ~/dotfiles/vim/.nvimrc ~/.config/nvim/init.vim
